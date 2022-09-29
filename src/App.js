17ee0a36d5250a18a1323c35342676c7ca180628
Nah Wu <nah.wu@stengg.com>
2022-09-29 21:04:44 +0800

    Added nav menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import DashboardPanel from "./components/DashboardPanel";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <DashboardPanel />
     </div>
   );
 }
